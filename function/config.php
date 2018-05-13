92a924a3a6503f5acf5e2bfb6d23b2ad4a5e8143
廉晟 <1456457523@qq.com>
2018-05-13 23:22:52 +0800

    添加了一些内容

--- function/config.php
+++ function/config.php
@@ -6,26 +6,17 @@
  * Time: 19:31
  */
 //define('SERVER','http://192.169.10.15:8000/api/');
-define('SERVER','http://192.169.10.11/iot3/function/api.php');
+
+$lc="http://".$_SERVER['HTTP_HOST']."/iot3/function/api.php";
+
+define('SERVER',$lc);
 function GetJson($url){
     $port_data = file_get_contents($url);
     $port_arr = json_decode($port_data,true);
     return $port_arr;
 }
 
-//function passwd($Str,$Check_data){
-//    $data = array();
-//    $Token=(int)date("Hi");
-//    for ($x=-1; $x<=1; $x++) {
-//        $data[]= base64_encode(hash_hmac("SHA1",$Str,$x+$Token, true));
-//    }
-//    if (in_array($Check_data, $data))
-//        {
-//            return 1;
-//        }else{
-//        return 0;
-//        }
-//    }
+
 function tok($str){
     $Token=date("Hi");
     $data= base64_encode(hash_hmac("SHA1",$str,$Token,true));
